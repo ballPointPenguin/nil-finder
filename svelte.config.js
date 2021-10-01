8d6a678571f370c574ba04047fcea2071c6fe453
Benjamin Rosas <ben@aliencyb.org>
2021-10-01 05:32:24 -0500

    add node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,17 @@
+import adapter from '@sveltejs/adapter-node';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+    adapter: adapter({
+      // default options
+      out: 'build',
+      precompress: false,
+      env: {
+        host: 'HOST',
+        port: 'PORT'
+      }
+    }),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
